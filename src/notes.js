bb974136c707ea526b84f9d1946a66dbc37156a9
dihanto <dihanto2306@gmail.com>
2023-09-11 16:20:34 +0700

    ✨ finish api notes from dicoding

---
+++ src/notes.js
@@ -0,0 +1,8 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+/* eslint-disable eol-last */
+/* eslint-disable import/no-extraneous-dependencies */
+
+const notes = [];
+
+module.exports = notes;
